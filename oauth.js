99346ddf317901de6d8ea3371eb32ac53720ef39
Evan Dorsky <evan.dorsky@icloud.com>
2015-02-16 23:10:23 -0500

    Use env variables

---
+++ oauth.js
@@ -0,0 +1,7 @@
+module.exports = {
+	facebook: {
+		clientID: process.env.FACEBOOK_clientID,
+		clientSecret: process.env.FACEBOOK_clientSecret,
+		callbackURL: process.env.FACEBOOK_callbackURL
+	}
+}
